281d07cd85d7efa7581ba1b62668b91b4213ac48
nickdeacon <nickdeacon@btconnect.com>
2025-04-03 08:56:27 +0100

    Update binary_sensor.cpp

--- esphome/components/binary_sensor/binary_sensor.cpp
+++ esphome/components/binary_sensor/binary_sensor.cpp
@@ -33,7 +33,7 @@ void BinarySensor::send_state_internal(bool state, bool is_initial) {
   if (is_initial) {
     ESP_LOGD(TAG, "'%s': Sending initial state %s", this->get_name().c_str(), ONOFF(state));
   } else {
-    ESP_LOGD(TAG, "'%s': Sending state %s", this->get_name().c_str(), ONOFF(state));
+    ESP_LOGD(TAG, "'%s': My state %s", this->get_name().c_str(), ONOFF(state));
   }
   this->has_state_ = true;
   this->state = state;
